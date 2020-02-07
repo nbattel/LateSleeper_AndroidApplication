ad82097a9f3093ab9c44273e2ce82f2a369a1475
Milos Arsik <marsik@uwo.ca>
2020-02-07 17:08:49 -0500

    added firestore dependencies

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/MainActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/MainActivity.java
@@ -1,10 +1,13 @@
 package com.twelve.latesleeper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
