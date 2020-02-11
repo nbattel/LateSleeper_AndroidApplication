d327eebbec9322d1df7ca95e4985f96b42fd2b4b
Milos Arsik <marsik@uwo.ca>
2020-02-11 11:54:47 -0500

    main activity and non functional login created

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/MainActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/MainActivity.java
@@ -1,17 +1,29 @@
 package com.twelve.latesleeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.google.firebase.firestore.FirebaseFirestore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Access a Cloud Firestore instance from your Activity
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    // Variable to keep track of the amount of times the user touches the screen
+    private int touchCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // On click function to move to the log in activity
+    public void nextActivity(View view) {
+        touchCount  = touchCount  + 1;
+
+        // If the user taps twice, the activity will change to the login screen
+        if (++touchCount == 2) {
+            Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+            startActivity(intent);
+        }
     }
 }
