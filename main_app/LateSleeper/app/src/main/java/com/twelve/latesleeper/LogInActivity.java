77662e65daba80ef27e71f814dd6b05666eb4dd6
Milos Arsik <marsik@uwo.ca>
2020-02-11 11:54:47 -0500

    main activity and non functional login created

---
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/LogInActivity.java
@@ -0,0 +1,25 @@
+package com.twelve.latesleeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    // On click function to log the user into their account
+    public void logIn(View view) {
+
+    }
+
+    // On click function to allow a new user to create an account
+    public void createAccount(View view) {
+
+    }
+}
