7da606068c35e5b0bc6ba53869b6a54fdcc06cd9
Christopher Devito <cdevito2@uwo.ca>
2020-02-21 18:16:59 -0500

    user and entry model classes

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/user/Entry.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/user/Entry.java
@@ -1,4 +1,21 @@
 package com.twelve.latesleeper.user;
 
 public class Entry {
+    String body;
+    String title;
+    String date;
+
+    public Entry(String body,String title,String date)
+    {
+        this.body = body;
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getBody() {return this.body; }
+    public String getTitle(){return this.title;}
+    public  String getDate(){return this.date;}
+    public void setBody(String body){this.body = body;}
+    public  void setTitle(String title){this.title = title;}
+    public  void setDate(String date){this.date = date;}
 }
