ce2e0dbe8b193339f41e85534c0704f6d6aac684
nbattel <nbattel@uwo.ca>
2020-02-27 14:01:52 -0500

    Adding screens empty workflow for 4-step process

---
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ReframeActivity.java
@@ -0,0 +1,14 @@
+package com.twelve.latesleeper.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.twelve.latesleeper.R;
+
+public class ReframeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reframe);
+    }
+}
