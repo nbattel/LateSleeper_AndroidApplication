2893b4a08c8ba80b8fd59f0c11fa2a8e8475f60f
nbattel <nbattel@uwo.ca>
2020-02-27 16:27:19 -0500

    Fininshed setting up workflow

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/RefocusActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/RefocusActivity.java
@@ -1,7 +1,11 @@
 package com.twelve.latesleeper.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import com.twelve.latesleeper.R;
 
 public class RefocusActivity extends AppCompatActivity {
@@ -10,5 +14,25 @@ public class RefocusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refocus);
+    }
+
+    //OnClick for the save button will trigger the next screen in the workflow
+    public void nextButtonRefocus(View view){
+        Intent intent = new Intent(RefocusActivity.this, RevalueActivity.class);
+        startActivity(intent);
+
+        Toast toast = Toast.makeText(getApplicationContext(),"Refocus Next Button Clicked",Toast.LENGTH_LONG);
+        toast.show();
+        //TODO
+        //We need to save all the information the user enters on this screen into the database on save
+    }
+
+    //OnClick for the cancel button will not save any information
+    public void backButtonRefocus(View view){
+        Intent intent = new Intent(RefocusActivity.this, ReframeActivity.class);
+        startActivity(intent);
+
+        Toast toast = Toast.makeText(getApplicationContext(),"Refocus Back Button Clicked",Toast.LENGTH_LONG);
+        toast.show();
     }
 }
