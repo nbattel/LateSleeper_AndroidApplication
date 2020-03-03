fdae9873cda792b188e8fd7709c808bccdf6c65b
Christopher Devito <cdevito2@uwo.ca>
2020-03-03 16:52:22 -0500

    quiz works mostly

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/WiseAdvocateActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/WiseAdvocateActivity.java
@@ -2,17 +2,97 @@ package com.twelve.latesleeper.activity;
 
 import android.app.AppComponentFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.twelve.latesleeper.R;
 
 public class WiseAdvocateActivity extends AppCompatActivity {
+   private QuestionLibrary questionLibrary = new QuestionLibrary();
+   private TextView mScoreView;
+   private TextView mQuestionView;
+   private TextView choice1;
+    private Button choice2;
+    private Button choice3;
+    private Button choice4;
+    private Button choice5;
+    private int score =0;
+    private int question =0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multiplechoice);
+        mScoreView = findViewById(R.id.score);
+        mQuestionView =findViewById(R.id.question);
+        choice1 = findViewById(R.id.choice1);
+        choice2 = findViewById(R.id.choice2);
+        choice3 = findViewById(R.id.choice3);
+        choice4 = findViewById(R.id.choice4);
+        choice5 = findViewById(R.id.choice5);
+        chooseQuestion();
+
+        choice1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                score = score +1;
+                updateScore(score);
+                chooseQuestion();
+            }
+        });
+        choice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                score = score +2;
+                updateScore(score);
+                chooseQuestion();
+            }
+        });
+        choice3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                score = score +3;
+                updateScore(score);
+                chooseQuestion();
+            }
+        });
+        choice4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                score = score +4;
+                updateScore(score);
+                chooseQuestion();
+            }
+        });
+        choice5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                score = score +5;
+                updateScore(score);
+                chooseQuestion();
+            }
+        });
+
+
+
     }
 
+    private void chooseQuestion(){
+        mQuestionView.setText(questionLibrary.getQuestion(question));
+       choice1.setText(questionLibrary.getChoice1(question));
+        choice2.setText(questionLibrary.getChoice2(question));
+       choice3.setText(questionLibrary.getChoice3(question));
+        choice4.setText(questionLibrary.getChoice4(question));
+        choice5.setText(questionLibrary.getChoice5(question));
+
+
+        question++;
+    }
+    private void updateScore(int point) {
+        mScoreView.setText("" + score);
+    }
 
 }
