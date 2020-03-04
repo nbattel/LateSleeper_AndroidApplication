f5d12c1c429feb3a4995037d480356118d4766ba
Christopher Devito <cdevito2@uwo.ca>
2020-03-04 14:53:59 -0500

    display current time added

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ReframeActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ReframeActivity.java
@@ -14,6 +14,7 @@ public class ReframeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reframe);
+
     }
 
     /*public void toMultipleChoice(View view)
--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ViewGoalActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ViewGoalActivity.java
@@ -1,6 +1,10 @@
 package com.twelve.latesleeper.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,21 +12,31 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.twelve.latesleeper.R;
 
+import java.util.Date;
+
 public class ViewGoalActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
-        setContentView(R.layout.activity_login);
-    }
 
-    @Override
+        private TextClock tClock;
+        private TextView tView;
+        private Button btn;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_view_goal);
+            tClock = findViewById(R.id.textClock1);
+
+
+
+        }
+
+
+   /* @Override
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
-    }
+    }*/
 }
