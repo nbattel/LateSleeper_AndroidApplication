0c637929ff898e2c82d32be2b016a3a496c290e9
Christopher Devito <cdevito2@uwo.ca>
2020-03-13 12:30:12 -0400

    some fixes

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ViewGoalActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ViewGoalActivity.java
@@ -1,42 +1,81 @@
 package com.twelve.latesleeper.activity;
 
+
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import android.widget.TextClock;
-import android.widget.TextView;
+
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.twelve.latesleeper.R;
 
-import java.util.Date;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+
+
 
 public class ViewGoalActivity extends AppCompatActivity {
-
+    TimePicker alarmTime;
     private FirebaseAuth mAuth;
+    TextClock currentTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_goal);
+        alarmTime = findViewById(R.id.timepicker);
 
 
-        private TextClock tClock;
-        private TextView tView;
-        private Button btn;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_view_goal);
-            tClock = findViewById(R.id.textClock1);
+        currentTime = findViewById(R.id.textclock);
+        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(),RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
 
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
 
+                if (currentTime.getText().toString().equals(AlarmTime())){
+                    r.play();
+                }else{
+                    r.stop();
+                }
+            }
+        }, 0, 1000);
+    }
 
+    public String AlarmTime(){
+
+        Integer alarmHours = alarmTime.getCurrentHour();
+        Integer alarmMinutes = alarmTime.getCurrentMinute();
+        String stringAlarmMinutes;
+
+        if (alarmMinutes<10){
+            stringAlarmMinutes = "0";
+            stringAlarmMinutes = stringAlarmMinutes.concat(alarmMinutes.toString());
+        }else{
+            stringAlarmMinutes = alarmMinutes.toString();
         }
+        String stringAlarmTime;
 
-
-   /* @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-    }*/
+        if(alarmHours>12){
+            alarmHours = alarmHours-12;
+            stringAlarmTime = alarmHours.toString().concat(":").concat(stringAlarmMinutes).concat("PM");
+        }else{
+            stringAlarmTime = alarmHours.toString().concat(":").concat(stringAlarmMinutes).concat("AM");
+        }
+        return stringAlarmTime;
+    }
 }
+
+
