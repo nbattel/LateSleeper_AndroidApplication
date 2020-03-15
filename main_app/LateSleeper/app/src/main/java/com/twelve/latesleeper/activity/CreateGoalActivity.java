b5f5da22418ed8ba858e42cb1c7893250fe1f994
Tom Fouxman <tfouxman@gmail.com>
2020-03-15 04:45:38 -0400

    Added New Goal layout

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/CreateGoalActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/CreateGoalActivity.java
@@ -1,6 +1,8 @@
 package com.twelve.latesleeper.activity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,21 @@ import com.twelve.latesleeper.R;
 
 public class CreateGoalActivity extends AppCompatActivity {
 
+    TextView YOUTextView;
+    TextView YOUTextView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_goal);
+        YOUTextView = (TextView) findViewById(R.id.YOUTextView);
+        YOUTextView2 = (TextView) findViewById(R.id.YOUTextView2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        YOUTextView.setText(Html.fromHtml("It is important that <font color='#D81B60' size=30sp'>YOU</font> realize a need for a change.", Html.FROM_HTML_MODE_LEGACY));
+        YOUTextView2.setText(Html.fromHtml("The first thing <font color='#D81B60' size=30sp'>YOU</font> need to do is jot down your current feelings.", Html.FROM_HTML_MODE_LEGACY));
     }
 }
---
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/RevalueJournalActivity.java
@@ -0,0 +1,16 @@
+package com.twelve.latesleeper.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.twelve.latesleeper.R;
+
+public class RevalueJournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revalue_journal);
+    }
+}
