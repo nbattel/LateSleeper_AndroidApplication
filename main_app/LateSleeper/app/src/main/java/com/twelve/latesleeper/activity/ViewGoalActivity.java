3fc48434a837162238de5cd4300017f7b50f9110
Tom Fouxman <tfouxman@gmail.com>
2020-03-16 17:16:19 -0400

    Added the view goal page for a clicked goal

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ViewGoalActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/ViewGoalActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -13,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.twelve.latesleeper.R;
 import com.twelve.latesleeper.model.Goal;
 
+import java.util.Date;
 import java.util.HashMap;
 
 public class ViewGoalActivity extends AppCompatActivity {
@@ -20,8 +22,23 @@ public class ViewGoalActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Goal goal;
     private HashMap<String, Object> goalInfo;
+
     ProgressBar loadingBar;
     ConstraintLayout dimLayout;
+
+    TextView sleepTimeTextView;
+    TextView daysTextView;
+    TextView daysCompletedTextView;
+    TextView totalHoursTextView;
+    TextView dateCreatedTextView;
+    TextView completedTextView;
+
+    private String sleepTime;
+    private Integer days;
+    private Integer daysCompleted;
+    private Integer totalHours;
+    private Date dateCreated;
+    private Boolean completed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +48,45 @@ public class ViewGoalActivity extends AppCompatActivity {
 
         loadingBar = (ProgressBar) findViewById(R.id.loadingBar);
         dimLayout = (ConstraintLayout) findViewById(R.id.dimLayout);
+
+        sleepTimeTextView = findViewById(R.id.sleepTimeTextView);
+        daysTextView = findViewById(R.id.daysTextView);
+        daysCompletedTextView = findViewById(R.id.daysCompletedTextView);
+        totalHoursTextView = findViewById(R.id.totalHoursTextView);
+        dateCreatedTextView = findViewById(R.id.dateCreatedTextView);
+        completedTextView = findViewById(R.id.completedTextView);
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
         dimLayout.setVisibility(View.VISIBLE);
         dimLayout.bringToFront();
+
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         goal = (Goal) getIntent().getSerializableExtra("goal");
         goalInfo = goal.getGoal();
-        //updateUI(currentUser);
+
+        sleepTimeTextView.setText("Sleep time: " + goalInfo.get("sleepTime").toString());
+        daysTextView.setText("Days for goal: " + goalInfo.get("days").toString());
+        daysCompletedTextView.setText("Days completed: " + goalInfo.get("daysCompleted").toString());
+        totalHoursTextView.setText("Total hours recorded: " + goalInfo.get("totalHours").toString());
+        dateCreatedTextView.setText("Created on  " + goalInfo.get("dateCreated").toString());
+        completedTextView.setText("Completed: " + goalInfo.get("completed").toString());
+
         dimLayout.setVisibility(View.GONE);
     }
 
     public void startGoal(View view) {
         Intent intent = new Intent(ViewGoalActivity.this, AlarmClockWakeUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void fourSteps(View view) {
+        Intent intent = new Intent(ViewGoalActivity.this, RelabelActivity.class);
         startActivity(intent);
     }
 
