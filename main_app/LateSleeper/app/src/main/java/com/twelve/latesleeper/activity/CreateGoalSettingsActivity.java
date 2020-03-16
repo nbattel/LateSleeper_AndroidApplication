31152db9affb39fa487b56ba1e2cb5e30666c350
Christopher Devito <cdevito2@uwo.ca>
2020-03-16 13:00:26 -0400

    minor changes

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/CreateGoalSettingsActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/CreateGoalSettingsActivity.java
@@ -1,5 +1,8 @@
 package com.twelve.latesleeper.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,13 +23,14 @@ import com.twelve.latesleeper.R;
 import com.twelve.latesleeper.database.Database;
 import com.twelve.latesleeper.model.Goal;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class CreateGoalSettingsActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
-
+    int mHourSleep, mMinSleep;
     private String time;
     private Integer days;
 
@@ -42,6 +46,8 @@ public class CreateGoalSettingsActivity extends AppCompatActivity {
              public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
                  // TODO Auto-generated method stub
                  time = String.valueOf(hourOfDay).toString() + ":" + String.valueOf(minute).toString();
+                 mHourSleep = hourOfDay; //when the user changes the time on the timepicker(big clock on screen), the change in minute and hour are store
+                 mMinSleep = minute;
              }
 
          });
@@ -74,7 +80,28 @@ public class CreateGoalSettingsActivity extends AppCompatActivity {
 
     public void SaveGoal(View view) {
         CollectionReference goalCollection = Database.getDatabase().document("users/" + currentUser.getUid()).collection("goals");
+        //moved notifications here
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Date date = new Date();
+        Calendar calAlarm = Calendar.getInstance();//for alarm
+        Calendar calNow = Calendar.getInstance();//for the current time
+        calAlarm.setTime(date);
+        calNow.setTime(date);
+        calAlarm.set(Calendar.HOUR_OF_DAY,mHourSleep);//setting the hour to the user entered hour through timepicker
+        calAlarm.set(Calendar.MINUTE,mMinSleep); //setting the minute to the user entered minute through timepicker
+        calAlarm.set(Calendar.SECOND,0);//just setting the alarm to go off  right when time changes to the specific minute
 
+        if(calAlarm.before((calNow)))
+        {
+
+            calAlarm.add(Calendar.DATE,1); //if its 8pm and you set alarm to 6:30pm it will happen the next day
+        }
+        Intent i = new Intent(CreateGoalSettingsActivity.this,MyBroadCastReceiverNotification.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(CreateGoalSettingsActivity.this,9,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        //9 is request code, its just random, and 0 is the flag
+        alarmManager.set(AlarmManager.RTC_WAKEUP,calAlarm.getTimeInMillis(),pendingIntent);
+
+        //now create goal
         Goal goal = new Goal(time, days, 0, 0, new Date() );
 
         goalCollection.add(goal.getGoal())
@@ -84,6 +111,7 @@ public class CreateGoalSettingsActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Toast toast = Toast.makeText(getApplicationContext(), "Congratulations on creating a goal!", Toast.LENGTH_LONG);
                             toast.show();
+                            //set up notifications
                             Intent intent = new Intent(CreateGoalSettingsActivity.this, ViewGoalsActivity.class);
                             startActivity(intent);
                         }
