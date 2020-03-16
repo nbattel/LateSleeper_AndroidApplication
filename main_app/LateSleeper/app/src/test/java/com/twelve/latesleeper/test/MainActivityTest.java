43b77127d181525aa5c858c0f2c6ffb7f7f6c10d
Milos Arsik <marsik@uwo.ca>
2020-03-16 16:28:37 -0400

    added create account and log in logout automated testing

---
+++ main_app/LateSleeper/app/src/test/java/com/twelve/latesleeper/test/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.twelve.latesleeper.test;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
