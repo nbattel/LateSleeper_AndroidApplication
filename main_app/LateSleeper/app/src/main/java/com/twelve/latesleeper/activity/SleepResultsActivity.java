24f9e6d70114480301c5c30eb47dbaa8baba9e4c
Christopher Devito <cdevito2@uwo.ca>
2020-03-16 21:43:49 -0400

    fixed total hours calculation

--- main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/SleepResultsActivity.java
+++ main_app/LateSleeper/app/src/main/java/com/twelve/latesleeper/activity/SleepResultsActivity.java
@@ -2,25 +2,48 @@ package com.twelve.latesleeper.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.twelve.latesleeper.R;
+import com.twelve.latesleeper.database.Database;
 
+import org.json.JSONObject;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class SleepResultsActivity extends AppCompatActivity {
 
     public long sleepTime;
-    public long wakeUpTime;
+    //public long wakeUpTime;
+    public String sleepTimeDB;
     TextView sleepResults;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_results);
+        mAuth = FirebaseAuth.getInstance();
        // Bundle bundle = getIntent().getExtras();
         //sleepTime = bundle.getLong("sleepTime");
         //wakeUpTime = bundle.getLong("wakeUpTime");
@@ -34,20 +57,69 @@ public class SleepResultsActivity extends AppCompatActivity {
                 TimeUnit.MILLISECONDS.toSeconds(totalSleep) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(totalSleep)));*/
 
+        Database.getDatabase().collection("users").document(mAuth.getUid()).collection("goals").document(ViewSpecificGoalActivity.goalID)
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    //if it succeeds
+                    DocumentSnapshot result = task.getResult();
+
+                    sleepTimeDB = String.valueOf(result.get("sleepTime"));
+                    Log.d("TEST", "onComplete: "+sleepTimeDB);
+                    String sleepTimeHourAndMinute[] = sleepTimeDB.split(":");
+                    String sleepHour = sleepTimeHourAndMinute[0];
+                    String sleepMin = sleepTimeHourAndMinute[1];
+                    Calendar now = Calendar.getInstance();
+                    int year = now.get(Calendar.YEAR);
+                    int month = now.get(Calendar.MONTH) + 1; // Note: zero based!
+                    int day = now.get(Calendar.DAY_OF_MONTH)-1 ;//sleeptime was yesterday
+                    int hour = Integer.parseInt(sleepHour);
+                    int minute = Integer.parseInt(sleepMin);
+                    String bedDate = year+"-"+month+'-'+day+' '+hour+':'+minute;
+                    Log.d("TEST2", "onComplete: "+bedDate);
+                    sleepResults.setText(bedDate);
+
+                    SimpleDateFormat bedDateTimeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                    try {
+                        Date parsedDate = bedDateTimeStamp.parse(bedDate);
+                        Timestamp timestamp = new Timestamp(parsedDate.getTime());
+                        Bundle bundle = getIntent().getExtras();
+                        sleepTime = bundle.getLong("wakeUpTime");
+
+                        sleepResults.setText("sleepTime: "+(sleepTime - timestamp.getTime()));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
 
 
-        sleepResults.setText("you slept: INSERT CALC Total! Congrats!");
+                }
+                else {
+                    //if it fails
+
+                }
 
 
+
+            }
+        });
+
+
+        //Log.d("TEST2", "onComplete: "+sleepTimeDB);
+        //sleepResults.setText("you slept: INSERT CALC Total! Congrats!"+sleepTimeDB);
+
+
+        Database.getDatabase().collection("users").document(mAuth.getUid())
+                .collection("goals").document(ViewSpecificGoalActivity.goalID)
+                .update(
+                        "totalHours", FieldValue.increment(1)//need to figure out
+                );
 
     }
 
     public void toRevalue(View view)
     {
         Intent intent = new Intent(SleepResultsActivity.this, RevalueActivity.class);
-
-
-
         startActivity(intent); //navigate to set alarm
     }
 }
